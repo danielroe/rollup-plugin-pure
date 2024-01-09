7361db1c2c9692e3fcfcc3278d4355902d4ec8d0
Justin Schroeder <justin@formkit.com>
2024-01-09 16:58:47 -0500

    feat: convert to unplugin

--- build.config.ts
+++ build.config.ts
@@ -2,4 +2,5 @@ import { defineBuildConfig } from 'unbuild'
 
 export default defineBuildConfig({
   rollup: { emitCJS: true },
+  externals: ['vite', 'rollup', 'esbuild', 'webpack'],
 })
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import MagicString from 'magic-string'
-import type { Plugin } from 'rollup'
 import type { FilterPattern } from '@rollup/pluginutils'
+import MagicString from 'magic-string'
+import { createUnplugin } from 'unplugin'
 import { createFilter } from '@rollup/pluginutils'
 import { stripLiteral } from 'strip-literal'
 
@@ -11,7 +11,7 @@ export interface PureAnnotationsOptions {
   exclude?: FilterPattern
 }
 
-export function PluginPure(options: PureAnnotationsOptions): Plugin {
+export const unpluginPure = createUnplugin((options: PureAnnotationsOptions) => {
   const FUNCTION_RE = new RegExp(
     `(?<!\\/\\* #__PURE__ \\*\\/ )(?<![a-zA-Z0-9_$])(${options.functions
       .join('|')
@@ -27,28 +27,33 @@ export function PluginPure(options: PureAnnotationsOptions): Plugin {
   const filter = createFilter(options.include, options.exclude)
 
   return {
-    name: 'nuxt:pure-annotations',
-    transform: {
-      order: 'post',
-      handler(code, id) {
-        if (!filter(id) || !FUNCTION_RE_SINGLE.test(code)) {
-          return
-        }
+    name: 'unplugin-pure',
+    transform(code, id) {
+      if (!filter(id) || !FUNCTION_RE_SINGLE.test(code)) {
+        return
+      }
 
-        const s = new MagicString(code)
-        const strippedCode = stripLiteral(code)
+      const s = new MagicString(code)
+      const strippedCode = stripLiteral(code)
 
-        for (const match of strippedCode.matchAll(FUNCTION_RE)) {
-          s.overwrite(match.index!, match.index! + match[0].length, '/* #__PURE__ */ ' + match[0])
-        }
+      for (const match of strippedCode.matchAll(FUNCTION_RE)) {
+        s.overwrite(match.index!, match.index! + match[0].length, '/* #__PURE__ */ ' + match[0])
+      }
 
-        if (s.hasChanged()) {
-          return {
-            code: s.toString(),
-            map: options.sourcemap ? s.generateMap({ hires: true }) : undefined,
-          }
+      if (s.hasChanged()) {
+        return {
+          code: s.toString(),
+          map: options.sourcemap ? s.generateMap({ hires: true }) : undefined,
         }
-      },
+      }
     },
   }
-}
+})
+
+/**
+ * Maintain backwards compatibility with existing rollup setup.
+ */
+export const PluginPure = unpluginPure.rollup
+export const esbuildPure = unpluginPure.esbuild
+export const vitePure = unpluginPure.vite
+export const webpackPure = unpluginPure.webpack
