246352e7e884d0f6c5683c90c8d281674a7722df
Daniel Roe <daniel@roe.dev>
2025-03-04 10:13:21 +0000

    test: add basic codspeed benchmark (#352)

--- vitest.config.mjs
+++ vitest.config.mjs
@@ -1,6 +1,9 @@
+import codspeedPlugin from '@codspeed/vitest-plugin'
+import { isCI } from 'std-env'
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
+  plugins: isCI ? [codspeedPlugin()] : [],
   test: {
     coverage: {
       reporter: ['text', 'json'],
