b916208d7070e1569c744a9caa0c90f765d50dda
kricsleo <kricsleo@163.com>
2025-03-04 15:36:42 +0800

    perf: reuse AST

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,11 @@ export interface PureAnnotationsOptions {
   exclude?: FilterPattern
 }
 
+interface Annotation {
+  start: number
+  annotation: string
+}
+
 const withLocations = <T>(node: T) => node as T & { start: number, end: number }
 export function PluginPure(options: PureAnnotationsOptions): Plugin {
   const functionSet = new Set(options.functions)
@@ -40,6 +45,7 @@ export function PluginPure(options: PureAnnotationsOptions): Plugin {
         }
 
         const s = new MagicString(code)
+        const annotations: Annotation[] = []
 
         walk(ast, {
           enter(_node) {
@@ -57,7 +63,10 @@ export function PluginPure(options: PureAnnotationsOptions): Plugin {
                   return
                 }
               }
-              s.prependRight(node.start, '/*@__NO_SIDE_EFFECTS__*/ ')
+              const annotation = '/*@__NO_SIDE_EFFECTS__*/ '
+              s.prependRight(node.start, annotation)
+              annotations.push({ start: node.start, annotation })
+              return
             }
 
             // Handle function calls - add @__PURE__ annotation
@@ -72,18 +81,51 @@ export function PluginPure(options: PureAnnotationsOptions): Plugin {
                   return
                 }
               }
-              s.prependRight(node.start, '/*@__PURE__*/ ')
+              const annotation = '/*@__PURE__*/ '
+              s.prependRight(node.start, annotation)
+              annotations.push({ start: node.start, annotation })
             }
           },
         })
 
-        if (s.hasChanged()) {
+        if (annotations.length) {
+          fixAst(ast, annotations)
+
           return {
             code: s.toString(),
+            ast: withLocations(ast),
             map: options.sourcemap ? /* v8-ignore */ s.generateMap({ hires: true }) : undefined,
           }
         }
       },
     },
   }
+}
+
+function fixAst(ast: Program, annotations: Annotation[]) {
+  walk(ast, {
+    enter(_node) {
+      let startOffset = 0
+      let endOffset = 0
+      const node = withLocations(_node)
+
+      for (const annotation of annotations) {
+        if (annotation.start <= node.start) {
+          startOffset += annotation.annotation.length
+        }
+        if (annotation.start <= node.end) {
+          endOffset += annotation.annotation.length
+        }
+      }
+
+      if (startOffset > 0 || endOffset > 0) {
+        node.start += startOffset
+        node.end += endOffset
+      }
+    },
+  })
+
+  // TODO: If the `ast.comments` should be updated,
+  // push the annotations to it here.
+  // If not, remove the `ast.comments` check above.
 }
